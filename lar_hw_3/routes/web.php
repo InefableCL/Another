5e760bb0673cf2b74281b25e46dfb8b9e062d97a
InefableCL <inefablecl@gmail.com>
2021-10-19 18:16:18 +0300

    hw_lar_3

---
+++ lar_hw_3/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/category/{number}', [\App\Http\Controllers\CategoryController::class, 'index'])->name('category');
+Route::get('/news/{number}', [\App\Http\Controllers\NewsController::class, 'index'])->name('news');
