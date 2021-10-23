f99216f40f1f986905d4bc924563647a00778e17
InefableCL <inefablecl@gmail.com>
2021-10-23 10:55:35 +0300

    hw_lar_4

---
+++ lar_hw_4/resources/views/category.blade.php
@@ -0,0 +1,20 @@
+@extends('home')
+
+@section('title')
+    {{ $category['title'] }}
+@endsection
+
+@section('content')
+    <h1 class="content__head">{{ $category['title'] }}</h1>
+    <p class="content__text">{{ $category['description'] }}</p>
+    <section class="content__news">
+        @forelse($news as $newsOne)
+            <div class="content__newsOne">
+                <h2 class="content__newsOneHead"><a href="{{ route('news', ['number' => $newsOne['id']]) }}" class="content__newsOneLink">{{ $newsOne['head'] }}</a></h2>
+                <p class="content__newsOneText">{{ $newsOne['text'] }}</p>
+            </div>
+        @empty
+            <p>Нет новостей</p>
+        @endforelse
+    </section>
+@endsection
