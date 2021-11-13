e0587514643826c76a886cee92fd62b49259c46f
InefableCL <inefablecl@gmail.com>
2021-11-13 12:23:37 +0300

    hw_lar_9

---
+++ lar_hw_9/resources/views/category.blade.php
@@ -0,0 +1,24 @@
+@extends('main')
+
+@section('title')
+    {{ $category->title }}
+@endsection
+
+@section('content')
+    <h1 class="content__head">{{ $category->title }}</h1>
+    <p class="content__text">{{ $category->inform }}</p>
+    <section class="content__news">
+        @forelse($news as $newsOne)
+            <div class="content__newsOne">
+                <h2 class="content__newsOneHead"><a href="{{ route('news', ['news' => $newsOne->id]) }}" class="content__newsOneLink">{{ $newsOne->title }}</a></h2>
+                <p class="content__newsOneText">{{ $newsOne->inform }}</p>
+                @if ($newsOne->link)
+                    <li><a class="mainmenu__link" href="{{ $newsOne->link }}">Посмотреть в источнике</a></li>
+                @endif
+            </div>
+        @empty
+            <p>Нет новостей</p>
+        @endforelse
+        {{ $news->links() }}
+    </section>
+@endsection
